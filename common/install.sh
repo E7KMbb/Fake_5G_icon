a386d5bede733cd6e4d079f4580e7884672a873c
E7KMbb <34418195+E7KMbb@users.noreply.github.com>
2020-04-19 10:13:58 +0800

    Add files via upload

--- common/install.sh
+++ common/install.sh
@@ -3,7 +3,8 @@ P7Z=$TMPDIR/common/tools/7za
 addfile=$TMPDIR/common/addfile
 chmod 755 $P7Z
 if [ -e /system/product/priv-app/SystemUI ]; then
-#Android10通常目录
+# Android10 following the usual directory
+# Android10通常目录
 sed -i "s/<device>/Android10Usually/g" $MODPATH/module.prop
 rm -rf $MODPATH/system/priv-app > /dev/null
 cp -f /system/product/priv-app/SystemUI/SystemUI.apk $TMPDIR/SystemUI.zip
@@ -11,7 +12,8 @@ $P7Z a $TMPDIR/SystemUI.zip $addfile/res
 cp $TMPDIR/SystemUI.zip $MODPATH/system/product/priv-app/SystemUI/SystemUI.apk
 else
 if [ -e /system/priv-app/SystemUI ]; then
-#Android10以下通常目录
+# Android10 below
+# Android10以下通常目录
 sed -i "s/<device>/Android10BelowUsually/g" $MODPATH/module.prop
 rm -rf $MODPATH/system/product > /dev/null
 cp -f /system/priv-app/SystemUI/SystemUI.apk $TMPDIR/SystemUI.zip
@@ -19,7 +21,8 @@ $P7Z a $TMPDIR/SystemUI.zip $addfile/res
 cp $TMPDIR/SystemUI.zip $MODPATH/system/priv-app/SystemUI/SystemUI.apk
 else
 if [ -e /system/product/priv-app/SystemUIGoogle ]; then
-#Google原生Android10
+# Google native Android10
+# Google原生Android10
 sed -i "s/<device>/GoogleNativeAndroid10/g" $MODPATH/module.prop
 rm -rf $MODPATH/system/priv-app > /dev/null
 cp -f /system/product/priv-app/SystemUIGoogle/SystemUIGoogle.apk $TMPDIR/SystemUIGoogle.zip
@@ -27,7 +30,8 @@ $P7Z a $TMPDIR/SystemUIGoogle.zip $addfile/res
 cp $TMPDIR/SystemUIGoogle.zip $MODPATH/system/product/priv-app/SystemUIGoogle/SystemUIGoogle.apk
 else
 if [ -e /system/priv-app/SystemUIGoogle ]; then
-#Google原生Android10以下
+# Google native Android10 below
+# Google原生Android10以下
 sed -i "s/<device>/GoogleNativeAndroid10Below/g" $MODPATH/module.prop
 rm -rf $MODPATH/system/product > /dev/null
 cp -f /system/priv-app/SystemUIGoogle/SystemUIGoogle.apk $TMPDIR/SystemUIGoogle.zip
@@ -35,7 +39,8 @@ $P7Z a $TMPDIR/SystemUIGoogle.zip $addfile/res
 cp $TMPDIR/SystemUIGoogle.zip $MODPATH/system/priv-app/SystemUIGoogle/SystemUIGoogle.apk
 else
 if [ -e /system/product/priv-app/OPSystemUI ]; then
-#一加氢、氧OS，Android10
+# OnePlus Android10
+# 一加氢、氧OS，Android10
 sed -i "s/<device>/OnePlusAndroid10/g" $MODPATH/module.prop
 rm -rf $MODPATH/system/priv-app > /dev/null
 cp -f /system/product/priv-app/OPSystemUI/OPSystemUI.apk $TMPDIR/OPSystemUI.zip
@@ -43,13 +48,16 @@ $P7Z a $TMPDIR/OPSystemUI.zip $addfile/res
 cp $TMPDIR/OPSystemUI.zip $MODPATH/system/product/priv-app/OPSystemUI/OPSystemUI.apk
 else
 if [ -e /system/priv-app/OPSystemUI ]; then
-#一加氢、氧OS，Android10以下
+# OnePlus Android10 below
+# 一加氢、氧OS，Android10以下
 sed -i "s/<device>/OnePlusAndroid10Below/g" $MODPATH/module.prop
 rm -rf $MODPATH/system/product > /dev/null
 cp -f /system/priv-app/OPSystemUI/OPSystemUI.apk $TMPDIR/OPSystemUI.zip
 $P7Z a $TMPDIR/OPSystemUI.zip $addfile/res
 cp $TMPDIR/OPSystemUI.zip $MODPATH/system/priv-app/OPSystemUI/OPSystemUI.apk
 else
+ui_print "Your device is not supported yet! "
+ui_print "Please tell the developer where your SystemUI.apk is located"
 abort "暂不支持你的设备！请告诉开发者你SystemUI.apk所在目录"
 fi
 fi

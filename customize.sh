8b688ab38baad06342d56eec76f644e7f28b4bdb
E7KMbb <2984227069@qq.com>
2021-02-09 08:38:52 +0000

    v5.0 Refactor, fix bug, support Android11

--- customize.sh
+++ customize.sh
@@ -1,69 +1,89 @@
-##########################################################################################
-#
-# MMT Extended Config Script
-#
-##########################################################################################
+#!/system/bin/sh
+SKIPUNZIP=1
 
-##########################################################################################
-# Config Flags
-##########################################################################################
+unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
 
-# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
-# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-#MINAPI=21
-#MAXAPI=25
-#DYNLIB=true
-#DEBUG=true
+# Default lang en_US
+LANG_VOLUME_KEY_ERROR="- Volume key error!"
+LANG_VOLUME_KEY_AGAIN="- Volume key not detected. Try again"
+LANG_SELECT_ATYLE="- Select Style: 5G or 5GE "
+LANG_SELECT_ATYLE_5G="- Vol Up = 5G "
+LANG_SELECT_ATYLE_5GE="- Vol Down = 5GE "
+LANG_SELECTED_5G="- Selected 5G "
+LANG_SELECTED_5GE="- Selected 5GE "
+LANG_WARNING="- Your device is not supported ! "
 
-##########################################################################################
-# Replace list
-##########################################################################################
+# Local lang
+locale=$(getprop persist.sys.locale|awk -F "-" '{print $1"_"$NF}')
+[[ ${locale} == "" ]] && locale=$(settings get system system_locales|awk -F "," '{print $1}'|awk -F "-" '{print $1"_"$NF}')
+if [ ${locale} = "zh_CN" ];then
+   . "$MODPATH/${locale}.ini"
+fi
 
-# List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
+chmod -R 0755 $MODPATH/file/tools
+P7Z=$MODPATH/file/tools/7za
 
-# Construct your list in the following format
-# This is an example
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here
-REPLACE="
-"
-
-##########################################################################################
-# Permissions
-##########################################################################################
-
-set_permissions() {
-  : # Remove this if adding to this function
-
-  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
-  # Some examples:
-  
-  # For directories (includes files in them):
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
-  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
-
-  # For files (not in directories taken care of above)
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
-  # set_perm /data/local/tmp/file.txt 0 0 644
+chooseport() {
+  # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  [ "$1" ] && local delay=$1 || local delay=3
+  local error=false
+  while true; do
+    timeout 0 $MODPATH/file/tools/$ARCH32/keycheck
+    timeout $delay $MODPATH/file/tools/$ARCH32/keycheck
+    local SEL=$?
+    if [ $SEL -eq 42 ]; then
+      return 0
+    elif [ $SEL -eq 41 ]; then
+      return 1
+    else
+      $error && abort "${LANG_VOLUME_KEY_ERROR}"
+      error=true
+      echo "${LANG_VOLUME_KEY_AGAIN}"
+    fi
+  done
 }
 
-##########################################################################################
-# MMT Extended Logic - Don't modify anything after this
-##########################################################################################
+ui_print "${LANG_SELECT_ATYLE}"
+ui_print "${LANG_SELECT_ATYLE_5G}"
+ui_print "${LANG_SELECT_ATYLE_5GE}"
+if chooseport; then
+  ui_print "${LANG_SELECTED_5G}"
+  addfile=$MODPATH/file/5G
+else
+  ui_print "${LANG_SELECTED_5GE}"
+  addfile=$MODPATH/file/5GE
+fi
 
-SKIPUNZIP=1
-unzip -qqo "$ZIPFILE" 'common/*' -d $TMPDIR >&2
-unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
-. $TMPDIR/functions.sh
+# Find dir
+find /system -name SystemUI.apk > $MODPATH/dir.txt
+find_dir_text=$(cat $MODPATH/dir.txt)
+if [[ ! ${find_dir_tex} ]]; then
+   rm -rf $MODPATH/dir.txt
+   find /system -name *SystemUI.apk > $MODPATH/dir.txt
+   find_dir_text=$(cat $MODPATH/dir.txt)
+   if [[ ! ${find_dir_tex} ]]; then
+      rm -rf $MODPATH/dir.txt
+      find /system -name SystemUI*.apk > $MODPATH/dir.txt
+      find_dir_text=$(cat $MODPATH/dir.txt)
+      if [[ ! ${find_dir_tex} ]]; then
+         abort "${LANG_WARNING}"
+      fi
+   fi
+fi
+
+# Inject file
+apkname=$(cat $MODPATH/dir.txt | awk -F '/' '{print $NF}')
+sed -i 's/\/'$apkname'//' $MODPATH/dir.txt
+dir=$(sed -n '1p' $MODPATH/dir.txt)
+mkdir -p $MODPATH$dir
+cp -f $dir/$apkname $TMPDIR/SystemUI.zip
+$P7Z a $TMPDIR/SystemUI.zip $addfile/res
+cp $TMPDIR/SystemUI.zip $MODPATH$dir/$apkname
+
+# Delete unnecessary files
+rm -rf $MODPATH/file
+rm -rf $MODPATH/dir.txt
+rm -rf $MODPATH/*.md
+
+set_perm_recursive $MODPATH 0 0 0755 0644
